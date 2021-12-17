e000cd990a0f2e1ca80b9809631cdef96d93a5bf
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-12-17 07:35:17 +0700

    change color & icon app

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -34,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: darkGrey,
+      backgroundColor: lightBlueGrey,
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(16.0),
